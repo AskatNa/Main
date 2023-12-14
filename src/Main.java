162ea06c16c130f9d09e7481141b4df9f3b07e18
Аскат Наринбетов <231308@astanait.edu.kz>
2023-12-14 10:35:37 +0600

    What number is bigger or are they equal?

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println(a);
+        int b = sc.nextInt();
+
+        if(a < b){
+            System.out.println("<");
+        } else if (a == b) {
+            System.out.println("=");
+        } else {
+            System.out.println(">");
+        }
+        }
     }
-}
